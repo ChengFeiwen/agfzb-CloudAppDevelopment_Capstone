783ab2756224554ab2441663f9cd2d7c8cc7d4b2
feiwencheng <frutagita@gmail.com>
2023-10-13 01:44:09 -0400

    add dockerfile

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,9 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","https://*.proxy.cognitiveclass.ai", "https://*.proxy.cognitiveclass.ai/*" ]
 
-CSRF_TRUSTED_ORIGINS = ["localhost","127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["localhost","127.0.0.1:8000","https://*.proxy.cognitiveclass.ai", "https://*.proxy.cognitiveclass.ai/*" ]
+                                                                                           https://*.proxy.cognitiveclass.ai/admin/login/?next=/admin/ 
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
